68d91593a31f18ecbf2510c12c030238d6997b79
FlavioPadilha <flavio_padilha_neto@msn.com>
2020-01-30 08:57:59 -0300

    Upload Projeto

---
+++ Akreditasi/dist/precache-manifest.829f8f1ffa249445ab1fa485743cce25.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "e97c9f16eb8b88b2611b",
+    "url": "/css/app.449a98b3.css"
+  },
+  {
+    "revision": "e97c9f16eb8b88b2611b",
+    "url": "/js/app.b1786d72.js"
+  },
+  {
+    "revision": "6aa38ca828161280fd76",
+    "url": "/js/chunk-151aa201.5f4c0d8d.js"
+  },
+  {
+    "revision": "291799bf17bb07ac137d",
+    "url": "/js/chunk-2d0cb6c9.d0299d2a.js"
+  },
+  {
+    "revision": "247e7bbd461a394a9750",
+    "url": "/js/chunk-2d2086b7.54af366c.js"
+  },
+  {
+    "revision": "29e6b9ecd14d4ca5cef4",
+    "url": "/js/chunk-74afe032.965ee9f3.js"
+  },
+  {
+    "revision": "d5a0dbfed0a89ee07546",
+    "url": "/js/chunk-vendors.6608ccd5.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "c4931cb31c08f584c7119d896ad3aeb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a794a15f6e45b941d8a521e7965221a8",
+    "url": "/img/brand/favicon.png"
+  },
+  {
+    "revision": "a80fae67d7ff34bf43621b5f00748bad",
+    "url": "/img/brand/green.png"
+  },
+  {
+    "revision": "755d2ebd6d3a19d6c831204b6b218a06",
+    "url": "/img/brand/white.png"
+  },
+  {
+    "revision": "1f533d2fdc40493ee834dd8e4288220c",
+    "url": "/img/theme/angular.jpg"
+  },
+  {
+    "revision": "d136a071cf09ba5ab8dc2ada1ab04015",
+    "url": "/img/theme/bootstrap.jpg"
+  },
+  {
+    "revision": "50dd5fdc839dfc16739b15370650485c",
+    "url": "/img/theme/react.jpg"
+  },
+  {
+    "revision": "3b8c576082e5ca27bae21a839f1290f6",
+    "url": "/img/theme/sketch.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "c532f6f0176968caa6265864ee1abb35",
+    "url": "/img/theme/vue.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "cee60cfd101f9b3d2aae0e819ec8b267",
+    "url": "/img/theme/profile-cover.jpg"
+  }
+];
